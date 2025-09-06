f0ef4fed8e3936b1c2eef884b45dbbacf76e0e3a
Ryan Algier <ralgier2811@gmail.com>
2025-09-06 14:37:33 -0400

    Fix with manual upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     print("Hello from perfect-bracket!")
 
+print('Adding something here to push')
+print('Adding something here to push')
 
+print("another commit test")
 if __name__ == "__main__":
     main()
